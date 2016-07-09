780dc387e754fd03c0b5b26c43f6df2f061f2263
RockStaR <subbareddyv.uoh@gmail.com>
2016-07-09 13:10:00 +0530

    Completed with images

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -62,7 +62,13 @@ HTMLActuator.prototype.addTile = function (tile) {
   this.applyClasses(wrapper, classes);
 
   inner.classList.add("tile-inner");
-  inner.textContent = tile.value;
+  //inner.textContent = tile.value;
+  img = document.createElement("img")
+  img_src = "images/photo" + tile.value + ".jpg";
+  img.setAttribute("src",img_src)
+  img.setAttribute("height", 106.5)
+  img.setAttribute("width", 106.5)
+  inner.appendChild(img)
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
